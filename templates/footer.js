d6c39503168007400c5c9199854176d0732ec6d0
Erika Tavera <dugarerika@gmail.com>
2020-10-15 23:18:54 +0300

    Ajusto footer y header

--- templates/footer.js
+++ templates/footer.js
@@ -2,7 +2,11 @@ export const templFooter = {
     render:(info) => {
 
         return`
-    
+        <ul>
+        <li><a href="https://web.facebook.com/" target="_blank" rel="noreferrer noopener" > <span class="icon-facebook"></span> Login with Facebook</a></li>
+        <li></li>
+        <li></li>
+        </ul>
         `
     }
 }
